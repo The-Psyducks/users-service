fd1f4f0a24248643d480a3fc68c2b42d086937b6
Monke <fjuarez@fi.uba.ar>
2024-09-09 19:23:46 -0300

    refactor: agregando varaibles para nomrbe de mensajes de errores

--- server/src/service/errors.go
+++ server/src/service/errors.go
@@ -5,3 +5,10 @@ import "errors"
 var (
 	ErrUserNotFound = errors.New("user not found")
 )
+
+var (
+	IncorrectUsernameOrPassword = "Incorrect username or password"
+	InternalServerError         = "Internal server error"
+	UsernameOrMailAlreadyExists = "Username or mail already exists"
+	UsernameNotFound            = "User not found"
+)
