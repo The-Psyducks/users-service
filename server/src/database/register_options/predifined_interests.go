4ceaf52d685f2ee0ba39845cc979c77a8753d4c9
Fracnisco Juarez <francisco.fjuarez@mercadolibre.com>
2024-11-05 16:14:14 -0300

    fix: sacando codigo innecesario

--- server/src/database/register_options/predifined_interests.go
+++ server/src/database/register_options/predifined_interests.go
@@ -1,7 +1,5 @@
 package register_options
 
-import "fmt"
-
 // Predefined interests
 var predefinedInterests = map[int]string{
 	0: "programming",
@@ -14,24 +12,6 @@ var predefinedInterests = map[int]string{
 // GetAllInterestsAndIds returns all predefined interests
 func GetAllInterestsAndIds() map[int]string {
     return predefinedInterests
-}
-
-func GetInterestsByIds(ids []int) ([]string, error) {
-	var interests []string
-	for _, id := range ids {
-		name := GetInterestName(id)
-		if name == "" {
-			return nil, fmt.Errorf("interest with ID %d not found", id)
-		}
-		interests = append(interests, name)
-	}
-	return interests, nil
-}
-
-// IsValidInterest checks if an interest ID is valid
-func IsValidInterest(id int) bool {
-	_, exists := predefinedInterests[id]
-	return exists
 }
 
 // GetInterestName returns the name of an interest given its ID or "" if the ID is invalid
