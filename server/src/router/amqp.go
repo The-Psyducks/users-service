56ad1660460eb595cfda3e29fdba33a2310ca830
Monke <fjuarez@fi.uba.ar>
2024-11-24 20:38:23 -0300

    fix: removing closing channel when creating it

--- server/src/router/amqp.go
+++ server/src/router/amqp.go
@@ -16,13 +16,11 @@ func CreateProducer() (*amqp.Channel, error) {
     if err != nil {
         return nil, fmt.Errorf("failed to connect to RabbitMQ: %v", err)
     }
-    defer conn.Close()
 
 	channel, err := conn.Channel()
     if err != nil {
         return nil, fmt.Errorf("failed to open a channel: %v", err)
     }
-    defer channel.Close()
 
     _, err = channel.QueueDeclare(queueName, true, false, false, false, nil)
     if err != nil {
